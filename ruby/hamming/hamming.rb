970507671d41750c1921e8f2c81438c522ee2e46
Deborah Hamel <deborahleehamel@gmail.com>
2016-01-27 02:22:49 -0700

    All but 2 tests passing.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,9 +2,9 @@ class Hamming
 
   def self.compute(string1, string2)
     hamming_distance = 0
-    string1.each_char.with_index do |base_pair_1, i|
+    string1.chars.each_with_index do |base_pair_1, i|
       base_pair_2= string2[i]
-      hamming_distance += 1 if base_pair_1 != base_pair_1 && base_pair_2 != nil
+      hamming_distance += 1 if base_pair_1 != string2.chars[i]
     end
 
     hamming_distance
