d09ffbed7754f71135d44a7e556fc2e7d93612d7
Deborah Hamel <deborahleehamel@gmail.com>
2016-01-28 15:44:12 -0700

    Test passing for contains 5 as prime factor.

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,6 +1,16 @@
 class Raindrops
 
   def self.convert(number)
-  return number.to_s
+    output = ""
+    if number % 3 == 0
+      output << "Pling"
+    end
+    if number % 5 == 0
+      output << "Plang"
+    end
+    if output.empty?
+      return number.to_s
+    end
+    return output
   end
 end
