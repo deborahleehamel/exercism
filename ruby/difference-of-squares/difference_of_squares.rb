cb99b1d106ebb09f40f622c30a93b49e11be9719
Deborah Hamel <deborahleehamel@gmail.com>
2016-01-28 17:32:13 -0700

    Difference method and all tests passing.

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,4 +1,5 @@
 class Squares
+  VERSION = 2
 
 # Computes difference between the sum of the squares and the
 # square of the sums of the first N natural numbers
@@ -10,18 +11,16 @@ attr_accessor :numbers
  # Returns the square of the sum of the first N natural numbers
   def square_of_sum
     ((0..@numbers).inject(0) { |sum, n| sum += n })**2
-    # numbers.map {|n| n*n}.inject(:+)
-    # sum = 0
-    # numbers.each { |num| sum += (num*num)}
-    # return sum
-  # return (@numbers * (@numbers + 1) / 2) ** 2
-  # numbers.inject(0) {|sum, num| sum + num*num }
   end
 
   def sum_of_squares
     (0..@numbers).inject(0) { |sum, n| sum += n**2 }
-  #     return @n * (@n + 1) * (2 * @n + 1) / 6
   end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
 
 
    # Returns the sum of the squares of the first N natural numbers
