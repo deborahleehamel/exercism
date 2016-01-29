dc81f9badb0e8674f9a8e200f113a7855c2bc4f8
Deborah Hamel <deborahleehamel@gmail.com>
2016-01-29 16:06:58 -0700

    Cleaned up comments and block.

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -2,26 +2,17 @@ class Fixnum
   VERSION = 1
 
   ARABIC = {
-     1000 => "M",
-     900 => "CM",
-     500 => "D",
-     400 => "CD",
-     100 => "C",
-     90 => "XC",
-     50 => "L",
-     40 => "XL",
-     10 => "X",
-     9 => "IX",
-     5 => "V",
-     4 => "IV",
-     1 => "I"
+        1000 => "M", 900 => "CM",
+         500 => "D", 400 => "CD",
+         100 => "C",  90 => "XC",
+          50 => "L",  40 => "XL",
+          10 => "X",   9 => "IX",
+           5 => "V",   4 => "IV",
+           1 => "I"
    }
 
-
-# convert normal numbers to Roman Numerals
   def to_roman
     n = self
-    # roman = []
     ARABIC.each_with_object([]) do |(k, v), roman |
       while n >= k
         roman << v
@@ -30,8 +21,3 @@ class Fixnum
     end.join
   end
 end
-
-
-
-# Roman.new.method => instance method
-# Roman.method => class method (self.)
