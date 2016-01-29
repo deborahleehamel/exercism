52d4be531d529db8428493d27e2514b493edb52b
Deborah Hamel <deborahleehamel@gmail.com>
2016-01-28 17:26:28 -0700

    Difference-of-squares beginning.

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,26 @@
+class Squares
+
+# Computes difference between the sum of the squares and the
+# square of the sums of the first N natural numbers
+attr_accessor :numbers
+  def initialize(numbers)
+    @numbers = numbers
+  end
+
+ # Returns the square of the sum of the first N natural numbers
+  def square_of_sum
+    ((0..@numbers).inject(0) { |sum, n| sum += n })**2
+    # numbers.map {|n| n*n}.inject(:+)
+    # sum = 0
+    # numbers.each { |num| sum += (num*num)}
+    # return sum
+  # return (@numbers * (@numbers + 1) / 2) ** 2
+  # numbers.inject(0) {|sum, num| sum + num*num }
+  end
+
+
+   # Returns the sum of the squares of the first N natural numbers
+
+
+
+end
