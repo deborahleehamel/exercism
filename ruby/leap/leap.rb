98736073340cb8fa10db09e8edf0c3f461de635e
Deborah Hamel <deborahleehamel@gmail.com>
2016-02-07 17:44:43 -0700

    Another iteration for leap year.

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,31 +1,32 @@
 class Year
+
   VERSION = 1
+
   def self.leap?(year)
-    new(year).leap?
+    self.new(year).leap?
   end
 
-  def initialize(year)
-    @year = year
+  attr_reader :reckoning
+
+  def initialize(reckoning)
+    @reckoning = reckoning
   end
 
   def leap?
-    (divisible_by?(4) && !divisible_by?(100)) || divisible_by?(400)
+    mundane_leap? || exceptional_century?
   end
 
   private
-  def divisible_by?(n)
-  	@year % n == 0
+
+  def mundane_leap?
+    reckoning % 4 == 0 && !century?
+  end
+
+  def century?
+    reckoning % 100 == 0
+  end
+
+  def exceptional_century?
+    reckoning % 400 == 0
   end
 end
-
-
-  # private
-  # def by4?
-  #   (year % 4) == 0
-  # end
-  # def by100?
-  #   (year % 100) == 0
-  # end
-  # def by400?
-  #   (year % 400) == 0
-  # end
