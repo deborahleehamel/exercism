a6e40b0e48bc70d8874d618e7e06f9450e9db6b7
Deborah Hamel <deborahleehamel@gmail.com>
2016-03-24 17:31:18 -0600

    Needed self.

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,10 +1,10 @@
 class Grains
 
-  def square(num)
+  def self.square(num)
     2 ** (num -1)
   end
 
-  def total
+  def self.total
     (0..64).reduce { |sum, num| sum + square(num)}
   end
 end
